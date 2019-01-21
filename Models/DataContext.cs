b3350c1d3af0b6efc83db7bc31994c3afbf4143a
guille <guille@localhost.localdomain>
2019-01-21 14:45:13 -0300

    database context

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -2,13 +2,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace rest_api_sistema_compra_venta.Models
 {
-    public class DataContext : DbContext
+    public class DataContext: DbContext
     {
         public DataContext(DbContextOptions<DataContext> options)
-            : base(options)
-        {
-        }
-
-        //public DbSet<TodoItem> TodoItems { get; set; }
+        :base(options){}
     }
 }
