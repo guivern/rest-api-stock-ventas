ba4bc7735b75f4d1c3a0dc81435829ee4f014d7f
guille <guille@localhost.localdomain>
2019-01-22 15:43:49 -0300

    modelo categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -8,12 +8,12 @@ namespace rest_api_sistema_compra_venta.Models
         public const int DESCRIPCION_MAX_LENGTH = 256;
 
         public long Id {get; set;}
-        [Required]
+        [Required(ErrorMessage="El nombre de categoría es requerido")]
         [MaxLength(NOMBRE_MAX_LENGTH, 
-        ErrorMessage="El nombre de categoría no debe tener más de ${NOMBRE_MAX_LENGTH} caracteres")]
+        ErrorMessage="El nombre de categoría no debe tener más de 50 caracteres")]
         public string Nombre {get; set;}
         [MaxLength(DESCRIPCION_MAX_LENGTH,
-        ErrorMessage="La descripción no puede tener más de ${DESCRIPCION_MAX_LENGTH} caracteres")]
+        ErrorMessage="La descripción no debe tener más de 256 caracteres")]
         public string Descripcion {get; set;}
         public bool Activo {get; set;}
     }
