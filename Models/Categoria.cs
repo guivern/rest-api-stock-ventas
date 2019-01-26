d5acd166631b0446bf8298508973188ab98e92ea
guille <guille@localhost.localdomain>
2019-01-26 11:48:05 -0300

    refactoring a la entidad categoria para que herede de SoftDeleteEntityBase

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -2,19 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace rest_api_sistema_compra_venta.Models
 {
-    public class Categoria
+    public class Categoria: SoftDeleteEntityBase
     {
         public const int NOMBRE_MAX_LENGTH = 50;
-        public const int DESCRIPCION_MAX_LENGTH = 256;
 
-        public long Id {get; set;}
-        [Required(ErrorMessage="El nombre de categoría es requerido")]
-        [MaxLength(NOMBRE_MAX_LENGTH, 
-        ErrorMessage="El nombre de categoría no debe tener más de 50 caracteres")]
+        [Required]
         public string Nombre {get; set;}
-        [MaxLength(DESCRIPCION_MAX_LENGTH,
-        ErrorMessage="La descripción no debe tener más de 256 caracteres")]
-        public string Descripcion {get; set;}
-        public bool Activo {get; set;}
     }
 }
