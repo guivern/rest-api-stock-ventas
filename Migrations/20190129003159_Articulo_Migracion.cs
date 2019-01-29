06f2880bf445f8625d3a7c098af5881b6f8895f2
guille <guille@localhost.localdomain>
2019-01-29 13:24:01 -0300

    se crea el modelo para la entidad articulo

---
+++ Migrations/20190129003159_Articulo_Migracion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace rest_api_sistema_compra_venta.Migrations
+{
+    public partial class Articulo_Migracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    FechaCreacion = table.Column<DateTime>(nullable: false),
+                    FechaModificacion = table.Column<DateTime>(nullable: true),
+                    Descripcion = table.Column<string>(nullable: true),
+                    Activo = table.Column<bool>(nullable: false),
+                    Codigo = table.Column<string>(nullable: true),
+                    Nombre = table.Column<string>(nullable: false),
+                    PrecioVenta = table.Column<decimal>(nullable: false),
+                    Stock = table.Column<int>(nullable: false),
+                    IdCategoria = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Articulos_Categorias_IdCategoria",
+                        column: x => x.IdCategoria,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articulos_IdCategoria",
+                table: "Articulos",
+                column: "IdCategoria");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulos");
+        }
+    }
+}
