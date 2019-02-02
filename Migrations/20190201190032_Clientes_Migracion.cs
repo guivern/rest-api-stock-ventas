39b4cb6f62d853a7b9861b90967f7a9d46a0c4e4
guille <guille@localhost.localdomain>
2019-02-02 09:27:08 -0300

    migracion clientes

---
+++ Migrations/20190201190032_Clientes_Migracion.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace rest_api_sistema_compra_venta.Migrations
+{
+    public partial class Clientes_Migracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    FechaCreacion = table.Column<DateTime>(nullable: false),
+                    FechaModificacion = table.Column<DateTime>(nullable: true),
+                    Descripcion = table.Column<string>(nullable: true),
+                    Activo = table.Column<bool>(nullable: false),
+                    Nombre = table.Column<string>(nullable: false),
+                    Apellido = table.Column<string>(nullable: false),
+                    TipoDocumento = table.Column<string>(nullable: true),
+                    NumeroDocumento = table.Column<string>(nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "FechaCreacion",
+                value: new DateTime(2019, 2, 1, 16, 0, 32, 61, DateTimeKind.Local).AddTicks(6862));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "FechaCreacion",
+                value: new DateTime(2019, 2, 1, 16, 0, 32, 64, DateTimeKind.Local).AddTicks(8343));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "FechaCreacion",
+                value: new DateTime(2019, 2, 1, 16, 0, 32, 64, DateTimeKind.Local).AddTicks(8403));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "FechaCreacion",
+                value: new DateTime(2019, 2, 1, 11, 15, 41, 186, DateTimeKind.Local).AddTicks(7106));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "FechaCreacion",
+                value: new DateTime(2019, 2, 1, 11, 15, 41, 190, DateTimeKind.Local).AddTicks(9002));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "FechaCreacion",
+                value: new DateTime(2019, 2, 1, 11, 15, 41, 190, DateTimeKind.Local).AddTicks(9129));
+        }
+    }
+}
